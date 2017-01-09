4ed9137206fbf0ff93061ead49c8d27ccaa82431
Sakthivel Balasubramaniam <sakthi836800@gmail.com>
2017-01-10 00:24:54 +0530

    HackerEarth find product of Big integer

---
+++ javaworkspace/src/com/hackerearth/java/basic/ArrayProduct.java
@@ -0,0 +1,39 @@
+package com.hackerearth.java.basic;
+
+import java.math.BigInteger;
+//import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ArrayProduct {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n;
+		Integer number;
+		BigInteger product = new BigInteger("1");
+		Scanner scan = null;
+		BigInteger power10 = new BigInteger("1000000007");
+		try
+		{
+			scan = new Scanner(System.in);
+			n = scan.nextInt();
+			for(int index = 0 ; index < n ; index++ )
+			{
+				number = scan.nextInt();
+				BigInteger temp = new BigInteger(number.toString());
+				product = product.multiply(temp);
+				product = product.mod(power10);
+			}
+			System.out.println(product);
+		}
+		catch(Exception e)
+		{
+			System.out.println("Error caused due to " + e);
+		}
+		finally
+		{
+			scan.close();
+		}
+	}
+
+}
